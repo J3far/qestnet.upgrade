727b3d15a58427a06fe2888c1b8dd0bc55155e2f
Darren Fidler <darren.fidler@spectraqest.com>
2014-10-24 15:19:59 +1030

    Preparation for release 1.0.1

--- publish-debug.bat
+++ publish-debug.bat
@@ -11,8 +11,8 @@ copy ".\QESTNET.Upgrade\QESTNET.Upgrade.UI\App.config.default" ".\temp\QESTNET.U
 copy ".\QESTNET.Upgrade\QESTNET.Upgrade.ScriptWriter.UI\App.config.default" ".\temp\QESTNET.Upgrade.ScriptWriter.UI.exe.config" /Y
 
 @REM  FIXME:  Get the version number automatically
-del /Q "\\ADLS0003\Development\Product Distribution\QESTNET.Upgrade\Debug\QESTNET.Upgrade v1.0.0.6.7z"
-"C:\Program Files (x86)\7-Zip\7z.exe" a -t7z "\\ADLS0003\Development\Product Distribution\QESTNET.Upgrade\Development\QESTNET.Upgrade v1.0.0.7.7z" ".\temp\*"
+del /Q "\\ADLS0003\Development\Product Distribution\QESTNET.Upgrade\Debug\QESTNET.Upgrade v1.0.1.0.7z"
+"C:\Program Files (x86)\7-Zip\7z.exe" a -t7z "\\ADLS0003\Development\Product Distribution\QESTNET.Upgrade\Development\QESTNET.Upgrade v1.0.1.0.7z" ".\temp\*"
 
 rmdir "./temp" /S /Q
 @pause
