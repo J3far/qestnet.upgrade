ad221aa1db18941a62c19dc68751e0cf9fc10d68
Darren Fidler <darren.fidler@spectraqest.com>
2015-04-24 15:01:37 +0930

    Removed publication of assemblies.

--- package_release.bat
+++ package_release.bat
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj /t:SetVersion;RebuildAll;Publish;Package /p:Configuration=Release;MajorVersion=1;MinorVersion=[DISABLED]
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj /t:SetVersion;RebuildAll;Package /p:Configuration=Release;MajorVersion=1;MinorVersion=[DISABLED]
 @pause
