394b1fce408422c47c35c8aa6b022a91cc76a35e
John Meegan <john.meegan@spectraqest.com>
2015-08-27 08:21:28 +0930

    BUG#4634 - Radiographic Reader Changes
    - Changed all captions to ASME/AWS Radiographic Reader Sheet.
    - Changed 'Size of Film' to a string.

--- Scripts/structure/structure.corrections.qn.sql
+++ Scripts/structure/structure.corrections.qn.sql
@@ -1510,6 +1510,12 @@ begin
 end
 go
 
+if exists (select 1 from information_schema.columns where table_schema = 'dbo' and table_name = 'InspectionRadiographicReaderSet' and column_name = 'SizeOfFilm')
+begin
+	alter table InspectionRadiographicReaderSet alter column SizeOfFilm nvarchar(15)
+end
+go
+
 if exists(select 1 from information_schema.columns where table_schema = 'dbo' and table_name = 'InspectionRadiographicReaderWeld' and column_name = 'WeldNumber')
 begin
 	alter table dbo.InspectionRadiographicReaderWeld alter column WeldNumber nvarchar(20)
