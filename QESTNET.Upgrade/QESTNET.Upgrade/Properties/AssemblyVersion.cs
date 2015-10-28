cab76f956d383652a08f5e7ae8465ac7b5dfb512
David Lucas <david.lucas@spectraqest.com>
2015-10-28 11:08:23 +1030

    Preparation for release v1.0.200

--- QESTNET.Upgrade/QESTNET.Upgrade/Properties/AssemblyVersion.cs
+++ QESTNET.Upgrade/QESTNET.Upgrade/Properties/AssemblyVersion.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.100.37532")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.200.30288")]
 
 
