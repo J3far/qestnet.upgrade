c354d914fd6055f58b5b86bbcdf9a9a449cac80c
David Lucas <david.lucas@spectraqest.com>
2015-12-18 13:53:34 +1030

    Prepare release v1.0.205

--- QESTNET.Upgrade/QESTNET.Upgrade.ScriptWriter.UI/Properties/AssemblyVersion.cs
+++ QESTNET.Upgrade/QESTNET.Upgrade.ScriptWriter.UI/Properties/AssemblyVersion.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.204.45778")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.205.37847")]
 
 
--- package_release.bat
+++ package_release.bat
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj /t:SetVersion;RebuildAll /p:Configuration=Release;MajorVersion=1;MinorVersion=0;Build=204
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj /t:SetVersion;RebuildAll /p:Configuration=Release;MajorVersion=1;MinorVersion=0;Build=205
 @pause
