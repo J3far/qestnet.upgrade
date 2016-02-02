9a235aa3d10cee8cec862bd8d84ab278d8fb410e
weiwenchi <weiwen.chi@spectraqest.com>
2016-02-02 11:30:12 +1030

    [axod: 5324] fix the issue which cannot define PRIMARY KEY constraint on nullable column

--- Scripts/structure/structure.corrections.qn.sql
+++ Scripts/structure/structure.corrections.qn.sql
@@ -1607,6 +1607,14 @@ begin
 end
 go
 
+-- Set existing null qestuuid to newid -- Bug 5324
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Tasks' AND COLUMN_NAME = 'QestUUID' AND IS_NULLABLE = 'YES')
+BEGIN
+	UPDATE dbo.qestNotifications SET QestUUID=NEWID() WHERE QestUUID IS NULL
+	ALTER TABLE dbo.qestNotifications ALTER COLUMN QestUUID uniqueidentifier NOT NULL
+END
+
+
 -- Set existing null qestuuid to guid.empty -- Bug 5324
 IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'qestNotifications' AND COLUMN_NAME = 'QestUUID' AND IS_NULLABLE = 'YES')
 BEGIN
