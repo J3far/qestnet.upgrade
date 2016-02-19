85534da3b0ed64841ec89ff97d31153fed4e56e2
David Lucas <david.lucas@spectraqest.com>
2016-02-19 11:12:50 +1030

    Prepare release v1.0.207

--- package_development.bat
+++ package_development.bat
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj /t:SetVersion;RebuildAll /p:Configuration=Development;MajorVersion=1;MinorVersion=0;Build=206
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj /t:SetVersion;RebuildAll /p:Configuration=Development;MajorVersion=1;MinorVersion=0;Build=207
 @pause
