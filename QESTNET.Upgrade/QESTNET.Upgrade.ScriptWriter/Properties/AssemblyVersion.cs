318a4abc26bb4118bf257ee1d988ecfb399f06c0
David Lucas <david.lucas@spectraqest.com>
2016-06-02 15:07:32 +0930

    Prepare release v1.0.209

--- QESTNET.Upgrade/QESTNET.Upgrade.ScriptWriter/Properties/AssemblyVersion.cs
+++ QESTNET.Upgrade/QESTNET.Upgrade.ScriptWriter/Properties/AssemblyVersion.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.208.37667")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.209.41139")]
 
 
